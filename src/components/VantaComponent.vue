8aa7df31c483ee22d871fc9754e6aebc666fab59
Syde2 <contact@syde2hub.com>
2024-03-14 21:59:00 +0100

    Init

---
+++ src/components/VantaComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+import VVanta from 'vue-vanta-revamp';
+
+const options = {
+	color: 0xa4647a,
+	backgroundColor: 0x403c46,
+	maxDistance: 31.00,
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+
+
+}
+</script>
+
+
+<template>
+	<div>
+		<v-vanta effect="net" :options="options"></v-vanta>
+	</div>
+</template>
+
+
+<style>
+#vanta-bg {
+	z-index: -1000;
+	position: absolute;
+	left: 0;
+	top: 0;
+	height: 100vh;
+	width: 100vw;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import VantaComponent from 'src/components/VantaComponent.vue'
+</script>
+
+<template>
+  <q-page class="flex flex-center">
+    <VantaComponent />
+    <H1>Hello</H1>
+
+  </q-page>
+</template>
