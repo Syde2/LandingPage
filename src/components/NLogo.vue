ce271967ea3ca886b2a46fc3aa34fed0649c6877
Syde2 <contact@syde2hub.com>
2024-03-18 20:25:28 +0100

    Update de la veille

--- src/components/NLogo.vue
+++ src/components/NLogo.vue
@@ -1,18 +1,38 @@
 <template>
-<div> 
-	<a  href="/">
-	<svg  viewBox="0 0 128 128" size="80" class="logo">
-		<path d="M87.102 118.131L42.667 61.255V45.473l49.247 63.665h26.463V19.492H73.87l-9.623-9.623H128v108.262z"></path>
-		<path d="M40.899 9.869l44.434 56.874v15.781L36.087 18.861H9.624v89.646h44.507l9.623 9.623H0V9.869z"></path>
-		</svg>
-		</a>
-		<span class="LogoCaption-ywgpa1-0 dGEFnz">Demo</span>
-		</div>
+  <div>
+    <a href="/">
+      <svg viewBox="0 0 128 128" size="80" class="logo">
+        <path d="M87.102 118.131L42.667 61.255V45.473l49.247 63.665h26.463V19.492H73.87l-9.623-9.623H128v108.262z">
+        </path>
+        <path d="M40.899 9.869l44.434 56.874v15.781L36.087 18.861H9.624v89.646h44.507l9.623 9.623H0V9.869z"></path>
+      </svg>
+    </a>
+    <span class="LogoCaption-ywgpa1-0 dGEFnz">Demo</span>
+  </div>
+
+  <div>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="logo" >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M16 11L17.6965 12.5268C19.239 13.9151 20.0103 14.6093 20.0103 15.5C20.0103 16.3907 19.239 17.0849 17.6965 18.4732L16 20"
+          stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path>
+        <path
+          d="M8.00005 4.82959L6.30358 6.35641C4.76102 7.74471 3.98975 8.43886 3.98975 9.32959C3.98975 10.2203 4.76102 10.9145 6.30358 12.3028L8.00005 13.8296"
+          stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path>
+        <path d="M13.9868 5L12.9934 8.70743M11.8432 13L10.0132 19.8297" stroke="#ffffff" stroke-width="1.5"
+          stroke-linecap="round"></path>
+      </g>
+    </svg>
+  </div>
+
+
 </template>
 
 
 <style>
-.logo{
-	fill: white;
+.logo {
+  fill: white;
 }
 </style>
