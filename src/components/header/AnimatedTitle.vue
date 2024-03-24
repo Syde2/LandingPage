e4bff7f798b6aa4d51c4c3dd0c0a797f0a51976d
Syde2 <contact@syde2hub.com>
2024-03-24 15:41:20 +0100

    Progress on rolling title

--- src/components/header/AnimatedTitle.vue
+++ src/components/header/AnimatedTitle.vue
@@ -1,13 +1,40 @@
 <script setup>
-import {ref, onMounted } from 'vue';
-import {timeline} from 'boot/gsap.js'
+import { ref, onMounted, computed } from 'vue';
+import { gsap, timeline } from 'boot/gsap.js'
 
 
-onMounted(()=>{
-  timeline.to('.title', {x:500})
-  timeline.to('.title', {x:0}, )
 
-} )
+const words = ['Hello', 'World', 'Lool' , 'Animation', 'Communauté', 'Developpeur', 'Internet', 'Consortium']
+const word = ref(words[0])
+const show = ref(true)
+
+
+function wordToggle(){
+  const wordIndex= words.indexOf(word.value)
+  if(words.length > 1){
+    words.splice(wordIndex,1)
+  }
+  word.value =  words[gsap.utils.random(0, words.length - 1, 1)]
+}
+
+function onBeforeEnter(el) {
+
+}
+
+function onEnter(el, done) {
+  console.log('Enter : ', el)
+  timeline.to(el, {
+    duration: 0.3, rotationX: 90, transformOrigin:
+      "50% 50% -50", stagger:  {each:0.02, ease:"power2", from:"start"},
+      onComplete: done
+  })
+}
+
+function onAfterEnter(el) {
+  wordToggle()
+  console.log('words : ', words)
+}
+
 
 
 
@@ -15,9 +42,21 @@ onMounted(()=>{
 
 
 <template>
-	<div>
-    <h1  class="title"> ACDC</h1>
-		<p  class="text-grey-5 text-overline subtitle"> Join the Club. Connect the dots ... </p>
 
-	</div>
+  <Transition appear @before-enter="onBeforeEnter" @enter="onEnter" @after-enter="onAfterEnter" :css="false">
+    <h1  :key=words.length class="title"> {{ word }} </h1>
+  </Transition>
+
+
+
 </template>
+
+<style>
+.gsap-box {
+  background: #42b883;
+  margin-top: 20px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,11 @@
 <script setup>
 import AnimatedTitle from 'src/components/header/AnimatedTitle.vue'
-import FridgeMagnets from 'src/components/header/FridgeMagnets.vue'
 
 </script>
 
 <template>
   <q-page class="flex flex-center column">
-    <!-- <AnimatedTitle /> -->
+    <AnimatedTitle />
   </q-page>
 </template>
 
